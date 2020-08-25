4895af68f015fab71e1ab4d52f897896040713a7
Pedro Silva <61997113+Pedro050700@users.noreply.github.com>
2020-08-25 13:06:02 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,86 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import "./styles.css";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function App() {
+  const [input, setInput] = useState("");
+  const calButtons = [];
+  [9, 8, 7, 6, 5, 4, 3, 2, 1, 0, ".", "%"].forEach(item => {
+    calButtons.push(
+      <button
+        onClick={b => {
+          setInput(input + b.target.value);
+        }}
+        value={item}
+        key={item}
+      >
+        {" "}
+        {item}
+      </button>
+    );
+  });
+
+  return (
+    <div className="wrapper">
+      {" "}
+      <div className="show-input">{input}</div>
+      <div className="digits flex">{calButtons}</div>
+      <div className="modifiers subgrid">
+        {/* clear button */}
+
+        <button onClick={() => setInput(input.substr(0, input.length - 1))}>
+          Clear
+        </button>
+
+        {/* clear all */}
+        <button onClick={() => setInput("")} value="">
+          AC
+        </button>
+      </div>
+      <div className="operations subgrid">
+        {/* add button */}
+        <button onClick={b=> setInput(input + b.target.value)} value="+">
+          +
+        </button>
+
+        {/* minus btn */}
+        <button onClick={b => setInput(input + b.target.value)} value="-">
+          {" "}
+          -{" "}
+        </button>
+
+        <button onClick={b => setInput(input + b.target.value)} value="*">
+          {" "}
+          x
+        </button>
+
+        <button onClick={b => setInput(input + b.target.value)} value="/">
+          {" "}
+          /
+        </button>
+        {/* "=" btn */}
+        <button
+          onClick={b => {
+            try {
+              setInput(
+                String(eval(input)).length > 3 &&
+                  String(eval(input)).includes(".")
+                  ? String(eval(input).toFixed(4))
+                  : String(eval(input))
+              );
+            } catch (b) {
+              console.log(b);
+            }
+          }}
+          value="="
+        >
+          =
+        </button>
+      </div>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
